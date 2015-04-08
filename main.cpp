e81cce2edd2d0468d10aaed63941889489576a44
oli001 <oli001@ucr.edu>
2015-04-08 21:25:53 +0000

    print 10 times

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!"
-    cout << endl;
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Hello, World!"
+        cout << endl;
+    }
     
     return 0;
 }
